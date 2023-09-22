dba8638bc17090b0e73b6b51367e123ade4fc31a
Александр <discegaudere113399@gmail.com>
2023-09-23 02:42:48 +0300

    fourth commit

--- 003.py
+++ 003.py
@@ -5,4 +5,4 @@ bonus = ["10.25%", "12.57%", "666.03%"]
 # print([i for i in "".join(bonus).split("%") if i != ""])
 
 print(
-    {f"{i}": j * float(k) for i, j, k in zip(names, base_salary, [i for i in "".join(bonus).split("%") if i != ""])})
+    {f"{i}": j * float(k)/100 for i, j, k in zip(names, base_salary, [i for i in "".join(bonus).split("%") if i != ""])})
